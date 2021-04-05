343866cae45c460299045e7674e8aae05b0bfc9b
Nitesh <niteshkhatri545@gmail.com>
2021-04-05 21:43:32 +0530

    git init

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const hbs = require("hbs") 
+const path = require("path")
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+let dirPath = path.resolve("../");                     // path of main directory
+const views = path.join(dirPath, "./app/views")        // path for view
+const partials = path.join(dirPath, "./app/partials"); // path for partials
+
+
+app.use(express.static('../public'));
+    
+
+app.set("view engine", "hbs");
+app.set("views", views);
+hbs.registerPartials(partials);     
+     
+
+// Website routes
+app.get("/", (req, res) => {
+    res.render("index")
+}); 
+                 
+app.get("/newsfeed", (req, res) => {
+    res.render("newsfeed")
+}); 
+  
+app.get("/about", (req, res) => {
+    res.render("about")
+});
+    
+app.get("/contact", (req, res) => {
+    res.render("contact")
+});
+
+app.get("/mission", (req, res) => {
+    res.render("mission")
+});  
+app.get("/gallery", (req, res) => {
+    res.render("imageGallery")
+});  
+   
+app.get("*", (req, res) => {
+    res.render("404")
+});
+
+  
+  
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
+
