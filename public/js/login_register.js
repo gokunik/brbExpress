c100f18e310370df24ac884ac8cecfb04fe05f60
Nitesh <niteshkhatri545@gmail.com>
2021-04-10 16:47:03 +0530

    homepage frontend work almost completed. few ui improvement remaining. favicon added.

---
+++ public/js/login_register.js
@@ -0,0 +1,44 @@
+
+
+// for registeration 
+// todo - needed work
+function validate() {
+    var uname = document.getElementById("fullName").value;
+    var email = document.getElementById("registerEmail").value;
+    var phone = document.getElementById("phoneNo").value;
+    var pass = document.getElementById("registerPassword").value;
+
+    var namechecker = /^[A-Za-z. ]{3,30}$/;
+    var emailchecker = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    var phonechecker = /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/;
+    var passchecker = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (namechecker.test(uname)) {
+        document.getElementById('nameerr').innerHTML = "";
+    } else {
+        document.getElementById('nameerr').innerHTML = "*Invalid name";
+        return false;
+    }
+
+    if (phonechecker.test(phone)) {
+        document.getElementById('phoneerr').innerHTML = "";
+    } else {
+        document.getElementById('phoneerr').innerHTML = "*Invalid number";
+        return false;
+    }
+
+    if (emailchecker.test(email)) {
+        document.getElementById('emailerr').innerHTML = "";
+    } else {
+        document.getElementById('emailerr').innerHTML = "*Invalid email";
+        return false;
+    }
+
+    if (passchecker.test(pass)) {
+        document.getElementById('passerr').innerHTML = "";
+    } else {
+        document.getElementById('passerr').innerHTML = "*Invalid password";
+        return false;
+    }
+
+}
