94373847f66234c2eae437a96685e7f316b0a90d
Nitesh <niteshkhatri545@gmail.com>
2021-04-14 21:55:18 +0530

    some changes in login and register module. now we can switch between login and register modal with button click. added donate modal and some minor change in contact page

--- public/js/header.js
+++ public/js/header.js
@@ -1,4 +1,22 @@
+// below code is for login and register modal
 
+let modal = document.getElementById("modalBody");
+let loginn = document.getElementsByTagName("template")[0].content.firstElementChild;
+let registerr = document.getElementsByTagName("template")[1].content.firstElementChild;
+
+function login() {
+    modal.innerHTML = "";
+    modal.appendChild(loginn);
+}
+
+function register() {
+    modal.innerHTML = "";
+    modal.appendChild(registerr);
+}
+
+
+// below code is for menu 
+// on scroll menu will stick to the top
 window.onscroll = function () { stickyHeader() };
 
 var header = document.getElementById("header");
@@ -11,14 +29,12 @@ function stickyHeader() {
     if (window.pageYOffset > sticky) {
         header.classList.add("stickyNavbar");
         header.classList.add("scroll-on");
-        infoHeader.style.marginBottom = header.offsetHeight+"px"
-        
+        infoHeader.style.marginBottom = header.offsetHeight + "px"
+
 
     } else {
         header.classList.remove("stickyNavbar");
         header.classList.remove("scroll-on");
         infoHeader.style.marginBottom = "0px"
     }
- 
-
 }
