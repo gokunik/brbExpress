3084e3ab97e57d38d91bd2c70bef8db9766a3803
Nitesh <niteshkhatri545@gmail.com>
2021-05-23 22:48:13 +0530

    dashboard is almost done from the frontend.

--- server/routers/router.js
+++ server/routers/router.js
@@ -8,15 +8,16 @@ const Volunteer = require("../models/Volunteer"); //Volunteer Schema is called h
 const Contact = require("../models/contactUs"); //Contact Us Schema is called here
 const Announce = require("../models/announ");//Announcement Schema
 
+
 //Initiating mailing Service 
 const transporter = nodemailer.createTransport({
-    service:'gmail',
+    service: 'gmail',
     tls: {
         rejectUnauthorized: false
     },
     auth: {
-        user:'krpalashish842@gmail.com',
-        pass:'axleblaze842'
+        user: 'krpalashish842@gmail.com',
+        pass: 'axleblaze842'
     }
 })
 
@@ -25,11 +26,10 @@ router.get("/", (req, res) => {
     res.render("index", failed = '');
 });
 
-router.get("/newsfeed", async(req, res) => {
+router.get("/newsfeed", async (req, res) => {
     try {
-        const anno = await Announce.find({}).sort({_id:-1}).limit(1);
-        console.log(anno);
-        res.render("newsfeed",{data:anno});
+        const anno = await Announce.find({}).sort({ _id: -1 }).limit(3);
+        res.render("newsfeed", { data: anno });
     } catch (e) {
         console.log("Error!!");
     }
@@ -51,6 +51,7 @@ router.get("/gallery", (req, res) => {
     res.render("imageGallery")
 });
 
+// routes for admin panel
 router.get("/admin-login", (req, res) => {
     res.render("./admin/adminLogin")
 });
@@ -59,8 +60,28 @@ router.get("/dashboard", (req, res) => {
     res.render("./admin/dashboard")
 });
 
+router.get("/admin-users", (req, res) => {
+    res.render("./admin/users")
+});
+
+router.get("/admin-newsfeed", async (req, res) => {
+    try {
+        const anno = await Announce.find({}).sort({ _id: -1 }).limit(3);
+        console.log(anno)
+        res.render("./admin/newsfeed", { data: anno });
+    } catch (e) {
+        console.log("Error!!");
+    }
+});
+
+router.get("/admin-contact", (req, res) => {
+    res.render("./admin/contact")
+});
+
+
+
 //Testing announcement api
-router.get('/ann',(req, res) => {
+router.get('/ann', (req, res) => {
     res.render("announce");
 });
 
@@ -71,85 +92,85 @@ router.get("*", (req, res) => {
 //This function will register user/volunteer in our website and store their data in database.
 router.post("/register", async (req, res) => {
     try {
-        User.findOne({ email: req.body.email }, function(err, user) {
-            if(err) {
-               console.log(err);//handle error here
+        User.findOne({ email: req.body.email }, function (err, user) {
+            if (err) {
+                console.log(err);//handle error here
             }
             //if a user was found, that means the user's email matches the entered email
             if (user) {
                 res.json("Email already exists as user");
             } else {
-                Volunteer.findOne({email:req.body.email},function(err,usere) {
-                if(err) {
-                    console.log(err);
-                }
-                if(usere) {
-                    res.json("Email already exists as volunteer");
-                } else  {
-                    //code if no user with entered email was found
-                    if (req.body.optradio == 'user') {
-                        const newUser = new User({
-                            name: req.body.name,
-                            phone: req.body.phone,
-                            email: req.body.email,
-                            password: req.body.password
-                        });
-                        const StoreUser = newUser.save();
-                        console.log(newUser);
-                        res.render("index");
-                
-                
-                    //Sending email to registered emailId
-                    transporter.sendMail({
-                        from:'krpalashish842@gmail.com',
-                        to:req.body.email,
-                        subject:'Test Mail',
-                        text:'Welcome to Banda Roti Bank Revolution'
-                    },(error,response)=> {
-                        if(error)
-                          console.log('Error',error);
-                        else
-                          console.log('Mail sent, ', response);
-                    })
-
-
-                    } else if (req.body.optradio == 'volunteer') {
-                        const newVolunteer = new Volunteer({
-                            name: req.body.name,
-                            phone: req.body.phone,
-                            email: req.body.email,
-                            password: req.body.password
-                        });
-                        const StoreVolunteer = newVolunteer.save();
-                        console.log(newVolunteer);
-                        res.render("index");
-                        
-                        //Sending email to registered emailId
-                     transporter.sendMail({
-                        from:'krpalashish842@gmail.com',
-                        to:req.body.email,
-                        subject:'Test Mail',
-                        text:'Welcome to Banda Roti Bank Revolution'
-                    },(error,response)=> {
-                        if(error)
-                          console.log('Error',error);
-                        else
-                          console.log('Mail sent, ', response);
-                    })
-
-                    } else {
-                        res.status(400).send("Bad request");
+                Volunteer.findOne({ email: req.body.email }, function (err, usere) {
+                    if (err) {
+                        console.log(err);
                     }
-                    
-                }
-            })
-        } 
+                    if (usere) {
+                        res.json("Email already exists as volunteer");
+                    } else {
+                        //code if no user with entered email was found
+                        if (req.body.optradio == 'user') {
+                            const newUser = new User({
+                                name: req.body.name,
+                                phone: req.body.phone,
+                                email: req.body.email,
+                                password: req.body.password
+                            });
+                            const StoreUser = newUser.save();
+                            console.log(newUser);
+                            res.render("index");
+
+
+                            //Sending email to registered emailId
+                            transporter.sendMail({
+                                from: 'krpalashish842@gmail.com',
+                                to: req.body.email,
+                                subject: 'Test Mail',
+                                text: 'Welcome to Banda Roti Bank Revolution'
+                            }, (error, response) => {
+                                if (error)
+                                    console.log('Error', error);
+                                else
+                                    console.log('Mail sent, ', response);
+                            })
+
+
+                        } else if (req.body.optradio == 'volunteer') {
+                            const newVolunteer = new Volunteer({
+                                name: req.body.name,
+                                phone: req.body.phone,
+                                email: req.body.email,
+                                password: req.body.password
+                            });
+                            const StoreVolunteer = newVolunteer.save();
+                            console.log(newVolunteer);
+                            res.render("index");
+
+                            //Sending email to registered emailId
+                            transporter.sendMail({
+                                from: 'krpalashish842@gmail.com',
+                                to: req.body.email,
+                                subject: 'Test Mail',
+                                text: 'Welcome to Banda Roti Bank Revolution'
+                            }, (error, response) => {
+                                if (error)
+                                    console.log('Error', error);
+                                else
+                                    console.log('Mail sent, ', response);
+                            })
+
+                        } else {
+                            res.status(400).send("Bad request");
+                        }
+
+                    }
+                })
+            }
 
         })
-     } catch (e) {
-            res.status(400).send(e);
-        }
-    });
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
 
 router.post('/login', async (req, res) => {
     try {
@@ -176,13 +197,13 @@ router.post('/login', async (req, res) => {
 });
 
 //This route is for storing the contact us form info to database.
-router.post("/contactUs",async(req,res) => {
+router.post("/contactUs", async (req, res) => {
     try {
         const newContact = new Contact({
-            name:req.body.senderName,
-            email:req.body.senderEmail,
-            phone:req.body.senderContact,
-            message:req.body.message
+            name: req.body.senderName,
+            email: req.body.senderEmail,
+            phone: req.body.senderContact,
+            message: req.body.message
         });
         const saveContact = await newContact.save();
         console.log(newContact);
@@ -193,28 +214,28 @@ router.post("/contactUs",async(req,res) => {
 });
 
 //Storing the announcement from admin portal to database 
-router.post('/anno', async(req, res) => {
+router.post('/anno', async (req, res) => {
     try {
         const newAnn = new Announce({
-            announce:req.body.announ
+            announce: req.body.announ
         });
-        const saveAnnoun = await newAnn.save(function(err,newAnn){
-            if(err){
+        const saveAnnoun = await newAnn.save(function (err, newAnn) {
+            if (err) {
                 console.log(err);
             } else {
                 console.log(newAnn);
             }
         });
-        res.render("newsfeed",{data: newAnn});
+        res.render("newsfeed", { data: newAnn });
     } catch (e) {
         console.log("Error!!");
     }
 });
 
 //this function is for admin login page validation
-router.post("/admin" ,async(req,res) => {
+router.post("/admin", async (req, res) => {
     try {
-        if(req.body.username === "axle" && req.body.password === "blaze"){
+        if (req.body.username === "axle" && req.body.password === "blaze") {
             res.render("admin/dashboard");
         } else {
             res.send("invalid");
